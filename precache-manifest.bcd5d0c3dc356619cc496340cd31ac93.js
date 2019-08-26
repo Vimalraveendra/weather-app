28af6dcd89708ca0b364663516f09ed553218474
vimal Raveendran <vimalraveendra@gmail.com>
2019-08-26 21:01:53 +0200

    Updates

---
+++ precache-manifest.bcd5d0c3dc356619cc496340cd31ac93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5842e245d159048bf8e182ceb1cde080",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3004a0d5af7a4c54e247",
+    "url": "/weather-app/static/css/main.b8bcfb89.chunk.css"
+  },
+  {
+    "revision": "02c32e75b750b5836f29",
+    "url": "/weather-app/static/js/2.58722e7c.chunk.js"
+  },
+  {
+    "revision": "3004a0d5af7a4c54e247",
+    "url": "/weather-app/static/js/main.94143ee2.chunk.js"
+  },
+  {
+    "revision": "7a390137f1e79ffd409f",
+    "url": "/weather-app/static/js/runtime~main.caa3d5bf.js"
+  },
+  {
+    "revision": "cea9c9014ed270c5d050f3b8380bd59b",
+    "url": "/weather-app/static/media/cloud.cea9c901.png"
+  }
+]);
